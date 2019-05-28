c8a56d022dba76430e12496c97532fbf11d51377
shimizuyudai <shimizu.yudai.0730@gmail.com>
2019-05-28 10:08:51 +0900

    update

---
+++ Assets/Klak/Spout/RenderTextureHolder2SpoutSender.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UtilPack4Unity;
+using Klak.Spout;
+
+public class RenderTextureHolder2SpoutSender : MonoBehaviour
+{
+    [SerializeField]
+    RenderTextureHolder renderTextureHolder;
+    [SerializeField]
+    SpoutSender spoutSender;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spoutSender.externalControl = true;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        var tex = renderTextureHolder.GetRenderTexture();
+        if (tex == null) return;
+        spoutSender.SendRenderTexture(tex);
+    }
+}
---
+++ Assets/Klak/Spout/SpoutReceiverTextureHolder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Klak.Spout;
+using UtilPack4Unity;
+
+public class SpoutReceiverTextureHolder : TextureHolderBase
+{
+    [SerializeField]
+    SpoutReceiver spoutReceiver;
+
+    public override Texture GetTexture()
+    {
+        return spoutReceiver.receivedTexture;
+    }
+}
